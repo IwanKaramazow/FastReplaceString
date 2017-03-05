6b21a9238327a0a1983d54c24f2219b9632a5872
Iwan <maxim@piesync.com>
2017-03-05 10:48:27 +0100

    Support wide && handle bigger files

--- replacestring.c
+++ replacestring.c
@@ -1,102 +1,127 @@
+#include <wchar.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <sys/stat.h>
-
 #include <string.h>
-#include <stddef.h>
+#include <limits.h>
+#include <locale.h>
+#include <time.h>
+
+/* Maximum default buffer size */
+#define MAXSTRSIZE 1000000 /* use 1 million bytes max */
+
+#if (__STDC_VERSION__ >= 199901L)
+  /* For C99 and above, include stdint.h and restrict max buffer size */
+  #include <stdint.h>
+  /* Don't allocate more than 1/3 of SIZE_MAX even if maximum default buffer
+   * setting is larger */
+  #define DEFSTRSIZE (SIZE_MAX / 3 > MAXSTRSIZE ? MAXSTRSIZE : SIZE_MAX / 3)
+  #define Z "z"
+  /* Don't cast size_t when printing it since we use C99's %z specifier */
+  #define PRTF_SZ_CAST 0 +
+#else
+  #define DEFSTRSIZE MAXSTRSIZE
+  /* Use the largest integral type as a printf specifier for size_t and
+   * cast size_t appropriately when doing so */
+  #define Z "l"
+  #define PRTF_SZ_CAST (unsigned long)
+#endif
+
+#define printusage fprintf(stderr, \
+"This program takes 4 parameters in this order: \n" \
+"filename old_token new_token number_of_iterations\n" \
+"It doesn't write to the file at all.\n" \
+"The file's encoding is treated according to your locale " \
+"i.e. in a Linux/UNIX environment, the LC_CTYPE or LC_ALL environment " \
+"variables. For UTF-8 encoding, one possibility is to set " \
+"LC_ALL to \"en_AU.UTF-8\".\n");
 
 #if (__STDC_VERSION__ >= 199901L)
 #include <stdint.h>
 #endif
 
-// source: http://creativeandcritical.net/str-replace-c
-char *repl_str(const char *str, const char *from, const char *to)
-{
-    /* Increment positions cache size initially by this number. */
-    size_t cache_sz_inc = 16;
-    /* Thereafter, each time capacity needs to be increased,
+wchar_t *repl_wcs(const wchar_t *str, const wchar_t *from, const wchar_t *to) {
+
+	/* Adjust each of the below values to suit your needs. */
+
+	/* Increment positions cache size initially by this number. */
+	size_t cache_sz_inc = 16;
+	/* Thereafter, each time capacity needs to be increased,
 	 * multiply the increment by this factor. */
-    const size_t cache_sz_inc_factor = 3;
-    /* But never increment capacity by more than this number. */
-    const size_t cache_sz_inc_max = 1048576;
+	const size_t cache_sz_inc_factor = 3;
+	/* But never increment capacity by more than this number. */
+	const size_t cache_sz_inc_max = 1048576;
 
-    char *pret, *ret = NULL;
-    const char *pstr2, *pstr = str;
-    size_t i, count = 0;
+	wchar_t *pret, *ret = NULL;
+	const wchar_t *pstr2, *pstr = str;
+	size_t i, count = 0;
 	#if (__STDC_VERSION__ >= 199901L)
-    	uintptr_t *pos_cache_tmp, *pos_cache = NULL;
+	uintptr_t *pos_cache_tmp, *pos_cache = NULL;
 	#else
-    	ptrdiff_t *pos_cache_tmp, *pos_cache = NULL;
+	ptrdiff_t *pos_cache_tmp, *pos_cache = NULL;
 	#endif
-    size_t cache_sz = 0;
-    size_t cpylen, orglen, retlen, tolen, fromlen = strlen(from);
+	size_t cache_sz = 0;
+	size_t cpylen, orglen, retlen, tolen, fromlen = wcslen(from);
 
-    /* Find all matches and cache their positions. */
-    while ((pstr2 = strstr(pstr, from)) != NULL){
+	/* Find all matches and cache their positions. */
+	while ((pstr2 = wcsstr(pstr, from)) != NULL) {
 		count++;
 
 		/* Increase the cache size when necessary. */
 		if (cache_sz < count) {
 			cache_sz += cache_sz_inc;
 			pos_cache_tmp = realloc(pos_cache, sizeof(*pos_cache) * cache_sz);
-			if (pos_cache_tmp == NULL){
-				goto end_repl_str;
-			}
-			else
-				pos_cache = pos_cache_tmp;
-
+			if (pos_cache_tmp == NULL) {
+				goto end_repl_wcs;
+			} else pos_cache = pos_cache_tmp;
 			cache_sz_inc *= cache_sz_inc_factor;
-
-			if (cache_sz_inc > cache_sz_inc_max){
+			if (cache_sz_inc > cache_sz_inc_max) {
 				cache_sz_inc = cache_sz_inc_max;
 			}
 		}
 
-		pos_cache[count - 1] = pstr2 - str;
+		pos_cache[count-1] = pstr2 - str;
 		pstr = pstr2 + fromlen;
-    }
+	}
 
-    orglen = pstr - str + strlen(pstr);
+	orglen = pstr - str + wcslen(pstr);
 
-    /* Allocate memory for the post-replacement string. */
-    if (count > 0){
-		tolen = strlen(to);
+	/* Allocate memory for the post-replacement string. */
+	if (count > 0) {
+		tolen = wcslen(to);
 		retlen = orglen + (tolen - fromlen) * count;
-    } else
-		retlen = orglen;
+	} else	retlen = orglen;
+	ret = malloc((retlen + 1) * sizeof(wchar_t));
+	if (ret == NULL) {
+		goto end_repl_wcs;
+	}
 
-    ret = malloc(retlen + 1);
-	
-    if (ret == NULL){
-		goto end_repl_str;
-    }
-
-    if (count == 0) {
+	if (count == 0) {
 		/* If no matches, then just duplicate the string. */
-		strcpy(ret, str);
-    } else {
-	/* Otherwise, duplicate the string whilst performing
+		wcscpy(ret, str);
+	} else {
+		/* Otherwise, duplicate the string whilst performing
 		 * the replacements using the position cache. */
 		pret = ret;
-		memcpy(pret, str, pos_cache[0]);
+		wmemcpy(pret, str, pos_cache[0]);
 		pret += pos_cache[0];
-		
-		for (i = 0; i < count; i++){
-			memcpy(pret, to, tolen);
+		for (i = 0; i < count; i++) {
+			wmemcpy(pret, to, tolen);
 			pret += tolen;
 			pstr = str + pos_cache[i] + fromlen;
-			cpylen = (i == count - 1 ? orglen : pos_cache[i + 1]) - pos_cache[i] - fromlen;
-			memcpy(pret, pstr, cpylen);
+			cpylen = (i == count-1 ? orglen : pos_cache[i+1]) - pos_cache[i] - fromlen;
+			wmemcpy(pret, pstr, cpylen);
 			pret += cpylen;
 		}
-		ret[retlen] = '\0';
-    }
+		ret[retlen] = L'\0';
+	}
 
-end_repl_str:
-    /* Free the cache and return the post-replacement string,
+end_repl_wcs:
+	/* Free the cache and return the post-replacement string,
 	 * which will be NULL in the event of an error. */
-    free(pos_cache);
-    return ret;
+	free(pos_cache);
+	return ret;
 }
 
 // source: https://www.joelonsoftware.com/2001/12/11/back-to-basics/
@@ -107,74 +132,136 @@ char* mystrcat( char* dest, char* src )
      return --dest;
 }
 
+/* Converts src from a multibyte string to a wide string, allocating memory for
+ * the wide string in the process, and returning that wide string. The memory
+ * can be deallocated with free() when no longer required.
+ */
+wchar_t *conv_mbtowcs(const char *src) {
+	size_t len = strlen(src);
+	wchar_t *ret;
+
+	/* Get length in wide chars. */
+	len = mbsrtowcs(NULL, &src, len+1, NULL);
+	if (len == (size_t)(-1)) {
+		perror("conv_mbtowcs: mbsrtowcs: encoding error");
+		return NULL;
+	}
+
+	/* Allocate memory for wide char string based on that length. */
+	if ((ret = malloc((len+1)*sizeof(wchar_t))) == NULL) {
+		perror("conv_mbtowcs: malloc");
+		return NULL;
+	}
+
+	/* Now perform the actual multibyte to wide string conversion. */
+	mbstowcs(ret, src, len+1);
+
+	return ret;
+}
+
 int main(int argc, char **argv){
 	// filename, src, dest = sys.argv[1:4]
-    /* check if amount of args is correct, otherwise exit */
-    if (argc != 4) {
-		fprintf(stderr, "`replacestring` except three arguments.\nUsage: replacestring filename src dest\n");
-		exit(1);
-    } else {
-		// filename_stage = filename + '.esy_rewrite'
-		// we know exactly how much to allocate: strlen filename + (".esy_rewrite") 12 + ('\0') 1 = 13
-		// TODO check if this is a proper reasoning ^^
-		char filename_stage[strlen(argv[1]) + 13];
-		char *p = filename_stage;
-		filename_stage[0] = '\0';
-		p = mystrcat(filename_stage, argv[1]);
-		p = mystrcat(filename_stage, ".esy_rewrite");
 
-		// filestat = os.stat(filename)
-		struct stat fileStat;
-		if(stat(argv[1], &fileStat) < 0){
-			exit(1);
-		}
+	FILE *f;
+	FILE *out;
+	char *mystr = NULL;
+	wchar_t *mywstr = NULL;
+	wchar_t *oldw, *neww, *newwstr = NULL;
+	const char *filename, *locale;
+	size_t r;
+	// long i;
+	long int filelen = DEFSTRSIZE;
 
-		// # TODO: we probably should handle symlinks too in a special way,
-		// # to modify their location to a rewritten path
-
-		// with open(filename, 'r') as input_file:
-		//     data = input_file.read()
-		/*
-			http://stackoverflow.com/questions/19260209/ftell-returning-incorrect-value
-			"You should get in a habit of always doing this, since only binary mode has well-defined behavior in standard C.
-			On POSIX systems, binary and text (default) mode behave the same,
-			but on windows, munging of newlines takes place in a way that messes up file contents and offsets."
-		*/
-		FILE *in = fopen(argv[1], "rb");
-		if(in == NULL) {
-			fprintf(stderr, "Could not open file: %s", argv[1]);
-			exit(1);
-		}
-		char original[fileStat.st_size + 1];
-		size_t len = fread(original, 1, fileStat.st_size, in);
-		// fread(...) does not zero-terminate a 'string''
-		original[len] = '\0';
-
-		// data = data.replace(src, dest)
-		char *data = repl_str(original, argv[2], argv[3]);
-		if(data == NULL){
-			fprintf(stderr, "Could not replace the `src` string with the `dest` string");
-			exit(1);
-		}
-
-		// with open(filename_stage, 'w') as output_file:
-		//     output_file.write(data)
-		FILE *out = fopen(filename_stage, "w");
-		if(out == NULL) {
-			fprintf(stderr, "Could not open file: %s", filename_stage);
-			exit(1);
-		}
-		fprintf(out,"%s", data);
-
-		// os.rename(filename_stage, filename)
-		rename(filename_stage, argv[1]);
-		// os.chmod(filename, stat.S_IMODE(filestat.st_mode))
-		chmod(argv[1], fileStat.st_mode);
-
-		// componentWillUnmount stuff
-		fclose(in);
-		fclose(out);
-
-		return 0;
+	/* check if amount of args is correct, otherwise exit */
+    if (argc <= 3) {
+		fprintf(stderr, "Not enough parameters.\n");
+		printusage;
+		exit(EXIT_FAILURE);
     }
+
+	/* Locale must be set so that mbstowcs() can interpret multibyte chars
+	 * according to that locale. Calling setlocale() with an empty string
+	 * causes the locale to be initialised from the environment in which the
+	 * program is run i.e. the user's locale, which in a Linux/UNIX
+	 * environment is specified in the LC_TYPE or LC_ALL variable.
+	 */
+	locale = setlocale(LC_CTYPE, "");
+	if (!locale)
+		perror("setlocale");
+
+	/* Parse and store command-line parameters */
+	filename = argv[1];
+	oldw = conv_mbtowcs(argv[2]);
+	if (!oldw)
+		exit(EXIT_FAILURE);
+	neww = conv_mbtowcs(argv[3]);
+	if (!neww)
+		exit(EXIT_FAILURE);
+
+	/* Try to determine the specified file's size so we know how much memory
+	 * to malloc. Note firstly that this requires opening the file in binary
+	 * mode, and secondly that at least the C89 version of the Standard does
+	 * not require that a binary stream supports seeking to the end of a
+	 * file. If the seek and tell fail, set the file size to DEFSTRSIZE.
+	 */
+	f = fopen(filename, "rb");
+	if (f == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+	if (fseek(f, 0, SEEK_END) > 0 || (filelen = ftell(f)) == -1L) {
+		filelen = DEFSTRSIZE;
+	}
+
+	/* Allocate enough memory to read in the specified file up to the size
+	 * determined above.
+	 */
+	if ((mystr = malloc(filelen + 1)) == NULL) {
+		perror("malloc (for file)");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Reopen the specified file in text mode. */
+	if (freopen(filename, "r", f) == NULL) {
+		perror("freopen");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Read in as much of specified file as possible */
+	if ((r = fread(mystr, 1, filelen, f)) == 0) {
+		perror("fread");
+		exit(EXIT_FAILURE);
+	}
+	mystr[r] = '\0';
+	if (fclose(f) != 0)
+		perror("fclose");
+
+	/* Convert file contents from multibyte to wide char. */
+	mywstr = conv_mbtowcs(mystr);
+	if (!mywstr)
+		exit(EXIT_FAILURE);
+
+	/* Free no-longer-required memory for mystr. */
+	free(mystr);
+
+	free(newwstr);
+	if ((newwstr = repl_wcs(mywstr, oldw, neww)) == NULL)
+		fwprintf(stderr, L"stringreplace returned NULL.\n");
+
+	char filename_stage[strlen(argv[1]) + 13];
+	char *p = filename_stage;
+	filename_stage[0] = '\0';
+	p = mystrcat(filename_stage, argv[1]);
+	p = mystrcat(filename_stage, ".esy_rewrite");
+
+	out = fopen(filename_stage, "w");
+	fwprintf(out, newwstr);
+
+	rename(filename_stage, argv[1]);
+	fclose(out);
+
+	/* Free allocated memory and exit with successful result */
+	free(newwstr);
+	free(mywstr);
+	return 0;
 }
